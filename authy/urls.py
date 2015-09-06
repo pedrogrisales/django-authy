ab9ffccf0774bbc9f3fb7e4eb11d51ba7dbff325
Pedro Grisales <grial313@gmail.com>
2015-09-06 18:48:29 -0500

    Update urls.py

--- authy/urls.py
+++ authy/urls.py
@@ -5,7 +5,7 @@ from django.conf.urls import patterns, include, url
 urlpatterns = patterns('',
     
     url(r'^login/$', views.login_user),
-    url(r'^logout/$', 'django.contrib.auth.views.logout'),
+    url(r'^logout/$', 'django.contrib.auth.views.logout_then_login'),
     
 )
 
